0c39e3048c908eabf58ab0b8516819692f1ca543
Karol <K_wlodarczyk@interia.pl>
2022-12-13 21:32:44 +0100

    end beginner

---
+++ app.js
@@ -0,0 +1,54 @@
+function addition (num1, num2){
+return num1 + num2
+}
+
+console.log(addition(3,5));
+
+function hoursintoSeconds(hour){
+ return hour * 60 * 60;
+}
+
+console.log(hoursintoSeconds(2)); 
+
+function calcPerimeter(lenght, widht){
+    return 2 * (lenght + widht);
+}
+
+console.log(calcPerimeter(10,20));
+
+function calcTriangleArea (base, height){
+    return 0.5 * base * height;
+}
+
+console.log(calcTriangleArea(20, 20));
+
+function appendFrontend(string){
+    return string + 'Frontend'
+}
+
+console.log(appendFrontend('Apple'));
+
+function sumGreaterThan100(number1, number2){
+    return number1 + number2 > 100
+}
+console.log(sumGreaterThan100(50,51));
+
+function lessThanOrEqualToZero(number3){
+    return number3 <= 0
+}
+
+console.log(lessThanOrEqualToZero(1))
+
+function oppositeBoolean(bool){
+    return !bool;
+}
+
+console.log(oppositeBoolean(true))
+
+function isNotZero(number4){
+    return number4 !== 0
+}
+
+console.log(isNotZero(5))
+
+function 
